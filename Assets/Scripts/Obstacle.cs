08dc76ede2d07af13d5a327128825c88af9b3fd6
oviePeriscope <habibur.rahman@periscopelabs.io>
2024-06-16 04:32:35 +0600

    Add obstacle collision

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+
+	public AudioSource audioSource;
+	public SpriteRenderer spriteRenderer;
+	public Collider2D collider;
+   
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		string collidedObjTag = collision.gameObject.tag;
+		//Debug.Log("tag    " + collidedObjTag);
+		if (collidedObjTag == "ball")
+		{
+			OnBallHit();
+		}
+	}
+
+
+	private void OnBallHit()
+    {
+		audioSource.Play();
+		spriteRenderer.enabled = false;
+		collider.enabled = false;
+	}
+
+}
